b232d5e0b4c67cc1744340d78e45b2cccb096462
volausson <veronicagolausson@gmail.com>
2022-05-24 12:09:15 +0000

    connect APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -13,23 +13,29 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('computer_quizz')
 
 
-def welcome():
-    """
-    Function defines start of game
-    """
+level1 = SHEET.worksheet('level1')
+
+data = level1.get_all_values()
+
+print(data)
+
+# def welcome():
+"""
+Function defines start of game
+"""
 
 
-print('Hi, welcome to my Computer Quizz!\n')
+# print('Hi, welcome to my Computer Quizz!\n')
 
-question = input('Are you ready to play (yes or no): \n').lower()
-answer = input('Are you ready to play? yes or no:\n')
-user_name = input()
-SCORE = 0
-TOT_Q = 5
+# question = input('Are you ready to play (yes or no): \n').lower()
+# answer = input('Are you ready to play? yes or no:\n')
+# user_name = input()
+# SCORE = 0
+# TOT_Q = 5
 
 
-if answer.lower() == 'yes':
-    answer = input('Please enter your name: ')
-    print('Welcome', user_name, ',please choose level 1, 2 or 3:')
-else:
-    print("Ok, maybe next time.")
+# if answer.lower() == 'yes':
+#    answer = input('Please enter your name: ')
+#    print('Welcome', user_name, ',please choose level 1, 2 or 3:')
+# else:
+#    print("Ok, maybe next time.")
