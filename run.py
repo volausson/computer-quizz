39e8f16c3abffeb2f26d3561d53afb47943f9ab2
volausson <veronicagolausson@gmail.com>
2022-08-30 15:45:04 +0000

    solve bug NameError, and prepare for deployement

--- run.py
+++ run.py
@@ -55,19 +55,20 @@ questions = [
 
 def start_quizz():
     """
-    Function defines the start of quizz
+    Function defines beginning of quizz and wishes Player
+    welcome.
     """
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     print('Hello, welcome to my Computer Quizz!\n')
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     ask = True
     while ask:
-        question = input('Are you ready to play (yes or no):\n').lower().strip()
-        if question == 'yes':
+        question = input('Are you ready to play?\nPlease enter y for yes or n for no:\n').lower().strip()
+        if question == 'y':
             enter_quizz()
         if question == '':
-            print('You need to type either yes or no to play!\n')
-        if question == 'no':
+            print('You need to type y for yes to play, or n for no to exit game!\n')
+        if question == 'n':
             print('~~~~~~~~~~~~~~~~~~~~~~')
             print('Okey, maybe next time.')
             print('~~~~~~~~~~~~~~~~~~~~~~')
@@ -77,20 +78,22 @@ def start_quizz():
 
 def enter_quizz():
     """
-    Function to engage user and give responsive feedback.
+    Function to engage user in quizz and give responsive feedback.
     """
-    player_name = input('Please enter your name(otherwise I will just call you player:\n').lower().strip()
+    player_name = input('Please enter your name, otherwise I will just call you Player:\n').lower().strip()
     if player_name == '':
-        player_name = 'player'
+        player_name = 'Player'
     print(f'Hi, {player_name}, here comes your first question...')
+    print('-----------------------------------------------------')
     play_quizz()
 
 
 def play_quizz():
     """
-    The Quizz begins.
+    Function defines start of quizz.
     """
     score = 0
+
     for question in questions:
         print(question['question'])
         question_options = question['options']
@@ -99,11 +102,19 @@ def play_quizz():
         player_choice = get_option_input()
         if player_choice == question['correct_answer']:
             score += 1
-            print('Well done, you got it right!\nYour current score is:', score)
+            print('----------------------------')
+            print('Well done, you got it right!\nScore:', score)
+            print('----------------------------')
+            print('')
+
         else:
             score -= 1
-            print('Sorry, wrong answer. You lost 1 point\nYour score is:', score)
-    end_game()
+            print('-------------------------------------')
+            print('Sorry, wrong answer. You lost 1 point\nScore:', score)
+            print('-------------------------------------')
+            print('')
+
+    end_game(score)
 
 
 def get_option_input():
@@ -111,21 +122,25 @@ def get_option_input():
     """
     Function to define players input for options of answers.
     """
-
     player_input = input('\nEnter either a/b/c:\n').lower()
     if player_input == "" or player_input not in ["a", "b", "c"]:
+        print('Incorrect input')
         return get_option_input()
     return player_input
 
 
-def end_game():
+def end_game(score):
     """
-    This ends the game and provides user a thank you and some information
+    This function ends the game, thank's Player for playing and informs
+    of final score.
     """
-    print("Thank you for playing my first python quiz game\n")
-    print('You scored', score, 'out of 5')
 
-    print('--------Game Over--------')
+    print('Thank you for playing, I hope you enjoyed the quizz.\n')
+    print('You scored', score, 'out of 5\n      Well done!!!')
+    print('')
+    print(' ----- GAME OVER -----')
+    print('')
+    print('')
 
 
 start_quizz()
